ead9c66d5408a2d17f7678d35e017c064a684dfd
0myyy <143873914+0myyy@users.noreply.github.com>
2024-08-29 22:27:50 +0530

    Add files via upload

---
+++ RE.js
@@ -0,0 +1,74 @@
+function a(){
+    document.getElementById("kk").src="./images/002.jpg"
+}
+
+function b(){
+    document.getElementById("kk").src="./images/003.jpg"
+}
+function c(){
+    document.getElementById("kk").src="./images/004.webp"
+}
+function d(){
+    document.getElementById("kk").src="./images/005.jpg"
+}
+function e(){
+    document.getElementById("kk").src="./images/006.webp"
+}
+function f(){
+    document.getElementById("kk").src="./images/007.jpg"
+}
+function g(){
+    document.getElementById("kk").src="./images/008.webp"
+}
+function h(){
+    document.getElementById("kk").src="./images/009.jpg"
+}
+function i(){
+    document.getElementById("kk").src="./images/001.jpg"
+}
+
+function j(){
+    document.getElementById("k2").src="./images/011.png"
+}
+
+function k(){
+    document.getElementById("k2").src="./images/012.webp"
+}
+function l(){
+    document.getElementById("k2").src="./images/013.webp"
+}
+function m(){
+    document.getElementById("k2").src="./images/014.webp"
+}
+function n(){
+    document.getElementById("k2").src="./images/015.webp"
+}
+function o(){
+    document.getElementById("k2").src="./images/010.jpg"
+}
+function p(){
+    document.getElementById("k3").src="./images/016.webp"
+}
+function q(){
+    document.getElementById("k3").src="./images/017.jpg"
+}
+function r(){
+    document.getElementById("k3").src="./images/018.webp"
+}
+function s(){
+    document.getElementById("k3").src="./images/019.jpg"
+}
+function t(){
+    document.getElementById("k3").src="./images/020.webp"
+}
+function u(){
+    document.getElementById("k3").src="./images/021.webp"
+}
+
+function v(){
+    document.getElementById("k4").src="./images/023.webp"
+}
+
+function w(){
+    document.getElementById("k4").src="./images/022.webp"
+}
